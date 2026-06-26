32aed1e64ba78cceb0d50d0671f467551915beac
Tristan914684 <tristanlyn44@gmail.com>
2026-06-26 20:38:20 +0800

    fix backend

--- TimeBros Backend/server.js
+++ TimeBros Backend/server.js
@@ -11,7 +11,8 @@ app.use(cors({
   origin: ["https://time-bros.vercel.app", "http://localhost:5173"],
   credentials: true,
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
